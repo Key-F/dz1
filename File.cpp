b1337ef1e409d8ffbf31069c94cb3192a52e6914
Key-F <kirill2010.97@mail.ru>
2015-04-01 00:29:00 +0400

    +Timeofcr, Timeofmod

--- File.cpp
+++ File.cpp
@@ -8,18 +8,24 @@ File::File();
     filename=filetype=filecreator=filesize=0;
 }
  
-File::File(const File& name, const File& type, const File& creator, int size) //Создание файла со всеми параметрами
+File::File(const File& name, const File& type, const File& creator, int size, int timec, int timem) //Создание файла со всеми параметрами
 {
     this->filename=name;
     this->filetype=type;
     this->filecreator=creator;
     this->filesize=size;
+	this->timeofcr=timec;
+	this->timeofmod=timem;
 }
  
-File::File(const File &Fl)
+File::File(const File &Fl)//конструктор копирования
 {
-	
-
+	this->filename=Fl.filename;
+    this->filetype=Fl.filetype;
+    this->filecreator=Fl.filecreator;
+    this->filesize=Fl.filesize;
+	this->timeofcr=Fl.timeofcr;
+	this->timeofmod=Fl.timeofmod;
 }
 
 File::~File() //Удаление файла
@@ -30,12 +36,16 @@ void File::setname(File& x) {this->filename=x;} //Задаем имя файла
 void File::settype(File& x) {this->filetype=x;} //Задаем тип файла
 void File::setsize(int x) {this->filesize=x;} //Задаем размер файла 
 void File::setcreator(File& x) {this->filecreator=x;} //Задаем имя создателя
+void File::settimec(int x) {this->timeofcr=x;} //Задаем время создания
+void File::settimem(int x) {this->timeofmod=x;} //Задаем время модификации
  
 string File::getname(File& x)const {return filename;} //Получаем имя файла
 string File::gettype(File& x)const {return filetype;} //Получаем тип файла
 int File::getsize(int x)const {return filesize;} //Получаем размер файла 
 string File::getcreator(File& x)const {return filecreator;} //Получем имя создателя
-std::ostream &operator<<(std::ostream &stream, File &H) //перегрузка оператора вЫвода <<
+int File::gettimec(int x)const {return timeofcr;} //Получаем время создания
+int File::gettimem(int x)const {return timeofmod;} //Получаем время модификации
+std::ostream &operator<<(std::ostream &stream, File &H) //Перегрузка оператора вывода <<
 {
     return stream << "File" <<
     "name=" << H.getname() << "," <<
@@ -43,3 +53,18 @@ std::ostream &operator<<(std::ostream &stream, File &H) //перегрузка оператора в
     "size=" << H.getsize() << "," <<
     "creator=" << H.getcreator();
 }
+File operator=(const File &);
+{
+	//swap
+}
+
+
+void File::swap(File & Fl)
+	{
+		std::swap(filename, Fl.filename);
+		std::swap(filecreator, Fl.filecreator);
+		std::swap(filesize, Fl.filesize);
+		std::swap(filetype, Fl.filetype);
+		std::swap(timeofcr, Fl.timeofcr);
+		std::swap(timeofmod, Fl.timeofmod);
+	}
--- File.h
+++ File.h
@@ -5,19 +5,28 @@ class File
 	string filecreator;
 	string filetype;
 	int filesize;
+	int timeofcr;//время создания
+	int timeofmod;//время последней модификации
 public:
 	File();
 	File(const File &Fl);// создание файла по типу другого
-	File(const File& name, const File& creator, const File& type, int size);
+	File(const File& name, const File& creator, const File& type, int size, int timeofcr, int timeofmod);
 	~File();// удаление файла
 	string getname()const;
 	string getcreator()const;
 	string gettype()const;
 	int getsize()const;
+	int gettimec()const;
+	int gettimem()const;
 	void setname(File& name);
 	void setcreator(File& creator);
 	void settype(File& type);
 	void setsize (int size);
+	void settimec (int timec);
+	void settimem (int timem);
 	friend std::ostream &operator<<(std::ostream &output, File &H);
     File operator+(const File &);
+	File operator=(const File &);
+	File operator==(const File &);
+	void swap(File & Fl);
 }
